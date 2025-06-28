1a8b82606a5dc213365bc7009ca03e11f2f12bca
Zhangjian He <hezhangjian97@gmail.com>
2025-06-28 21:50:04 +0800

    Initial commit

---
+++ src/main/java/com/example/JavaFxMain.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class JavaFxMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX");
+        Scene scene = new Scene(label, 1200, 800);
+
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
